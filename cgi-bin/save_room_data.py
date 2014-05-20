df67783c079c4e1b3d8ffc25632b0b55ae34de80
errollw <errollw@gmail.com>
2014-05-20 15:50:34 +0100

    Work on settings menu

--- cgi-bin/save_room_data.py
+++ cgi-bin/save_room_data.py
@@ -15,10 +15,10 @@ json_data = {};
 users = [];
 
 user_erroll = {};
-user_erroll['u_id'] = 'u_eww23'
-user_erroll['name'] = 'Erroll Wood'
-user_erroll['description'] = 'PhD student, master of netboards'
-user_erroll['img_src'] = 'http://eww23.user.srcf.net/img/profile_1x1_512.jpg'
+user_erroll['u_id'] = 'u_ls426'
+user_erroll['name'] = 'Lech Swirski'
+user_erroll['description'] = 'PhD student, tracks gaze'
+user_erroll['img_src'] = 'https://avatars3.githubusercontent.com/u/593597?s=460.jpg'
 
 users.append(user_erroll)
 
--- js/sb2.settings.initialize.js
+++ js/sb2.settings.initialize.js
@@ -19,29 +19,30 @@ function initializeUrlParams() {
 
 $(document).ready(function(){
 
-    initializeUrlParams();
-    if (urlParams['r_id']) r_id = urlParams['r_id'];
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
 
-    $.getJSON("content/room_data_"+r_id+".json", function(json_data){
+    // initializeUrlParams();
+    // if (urlParams['r_id']) r_id = urlParams['r_id'];
 
-    	var gap_between_users = 1980 / json_data.users.length;
-    	for (var i=0; i<json_data.users.length; i++){
-    		add_user(json_data.users[i], i*gap_between_users)
-    	}
-    });
+    // $.getJSON("content/room_data_"+r_id+".json", function(json_data){
+
+    // 	var gap_between_users = 1980 / json_data.users.length;
+    // 	for (var i=0; i<json_data.users.length; i++){
+    // 		add_user(json_data.users[i], i*gap_between_users)
+    // 	}
+    // });
     
 });
 
 
 function add_user(user, y_pos){
 
-	var header = $('<header/>').css('top', y_pos + 'px');
+	var usr_sec = $('<section/>').css('top', y_pos + 'px');
 	var img = $('<img/>').attr('src', user.img_src);
-	var sec_details = $('<section/>');
 	var p_name = $('<p/>').addClass('title').text(user.name);
 	var p_desc = $('<p/>').addClass('subtitle').text(user.description);
 
-	header.append(img).append(sec_details)
-	sec_details.append(p_name).append(p_desc)
-	$('body').append(header);
+	usr_sec.append(img).append(p_name).append(p_desc)
+	$('body').append(usr_sec);
 }
