f42fa29f08e2e19d3b62f3dcb4f8ddd0bc5f447f
errollw <errollw@gmail.com>
2014-05-27 15:27:51 +0100

    Added iframes to smartboards

--- js/sb2.network.initialize.js
+++ js/sb2.network.initialize.js
@@ -3,6 +3,8 @@ var urlParams;
 // Default to loading room SS20
 var r_id = "r_SS20";
 
+// distance in pixels between users
+var gap_between_users;
 
 function initializeUrlParams() {
 	var match,
@@ -24,7 +26,7 @@ $(document).ready(function(){
 
     $.getJSON("content/room_data_"+r_id+".json", function(json_data){
 
-    	var gap_between_users = 1980 / json_data.users.length;
+    	gap_between_users = 1980 / json_data.users.length;
     	for (var i=0; i<json_data.users.length; i++){
     		add_user(json_data.users[i], i*gap_between_users)
     	}
@@ -35,13 +37,27 @@ $(document).ready(function(){
 
 function add_user(user, y_pos){
 
+    // fist create the basic user components
 	var header = $('<header/>').css('top', y_pos + 'px');
 	var img = $('<img/>').attr('src', user.img_src);
 	var sec_details = $('<section/>');
 	var p_name = $('<p/>').addClass('title').text(user.name);
-	var p_desc = $('<p/>').addClass('subtitle').text(user.description);
+    var p_desc = $('<p/>').addClass('subtitle').text(user.description);
+	
+    // then add them
+    header.append(img).append(sec_details)
+    sec_details.append(p_name).append(p_desc)
+    $('body').append(header);
 
-	header.append(img).append(sec_details)
-	sec_details.append(p_name).append(p_desc)
-	$('body').append(header);
+    // if user has a webpage...
+    if (user.webpage_src) {
+        
+        var iframe = $('<iframe/>', {scrolling: "no", 'src': user.webpage_src});
+        iframe.css('top', y_pos + 'px').height(gap_between_users);
+        var header_bg = $('<div/>').addClass('header_bg').css('top', y_pos + 'px');
+
+        $('canvas').before(iframe);
+        $(header).before(header_bg);
+    }
+
 }
