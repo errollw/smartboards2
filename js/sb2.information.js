2f3798ddd4143d767ab22136a80895acd8b8eb8e
Sam Haines <samjhaines@gmail.com>
2014-07-18 15:17:23 +0100

    Added loading screen when loading information.html

--- js/sb2.information.js
+++ js/sb2.information.js
@@ -3,7 +3,9 @@
 $(function() {
 	
 	$("div.button#information").on("click", function() {
+		$.simpleMessage("show", "Loading...");
 		save(function() {
+			$.simpleMessage("destroy");
 			window.location.href = window.location.href.replace("board.html", "information.html");
 		});
 		
