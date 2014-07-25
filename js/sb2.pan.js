6c0f50df85bbdb76fa82ff682162b17c114900d3
Sam Haines <samjhaines@gmail.com>
2014-07-25 11:09:57 +0100

    Don't bother with panning if client is netboard

--- js/sb2.pan.js
+++ js/sb2.pan.js
@@ -2,40 +2,42 @@
 
 $(function() {
 	
-	// Is the user's finger/mouse dragging?
-	var isCurrentlyPanning = false, startPosition, initScrollTop, initScrollLeft;
+	// Only do panning if the client isn't a NetBoards
+	if (navigator.userAgent.indexOf("NetBoard/") == -1) {
+		// Is the user's finger/mouse dragging?
+		var isCurrentlyPanning = false, startPosition, initScrollTop, initScrollLeft;
 
-	
-	// Bind event handlers
-	$("canvas").on("mousedown touchstart", function(e) {
-		// Starting pan
-		if (edit_mode != "PANNING" || isCurrentlyPanning == true) return;
-		isCurrentlyPanning = true;
 		
-		startPosition = (e.type.indexOf("mouse") > -1) ? new Point(e.clientX, e.clientY) : new Point(e.originalEvent.touches[0].clientX, e.originalEvent.touches[0].clientY);
-		initScrollTop = $(document).scrollTop();
-		initScrollLeft = $(document).scrollLeft();
+		// Bind event handlers
+		$("canvas").on("mousedown touchstart", function(e) {
+			// Starting pan
+			if (edit_mode != "PANNING" || isCurrentlyPanning == true) return;
+			isCurrentlyPanning = true;
+			
+			startPosition = (e.type.indexOf("mouse") > -1) ? new Point(e.clientX, e.clientY) : new Point(e.originalEvent.touches[0].clientX, e.originalEvent.touches[0].clientY);
+			initScrollTop = $(document).scrollTop();
+			initScrollLeft = $(document).scrollLeft();
+			
+		}).on("mouseup mouseout touchend touchcancel touchleave", function(e) {
+			// Ending pan
+			if (edit_mode != "PANNING" || isCurrentlyPanning == false) return;
+			isCurrentlyPanning = false;
+			startPosition = null;
+		}).on("mousemove touchmove", function(e) {
+			// Perform the move
+			if (edit_mode != "PANNING" || isCurrentlyPanning == false) return;
+			
+			var currentPosition = (e.type.indexOf("mouse") > -1) ? new Point(e.clientX, e.clientY) : new Point(e.originalEvent.touches[0].clientX, e.originalEvent.touches[0].clientY);
+			
+			var delta = currentPosition.subtract(startPosition);
+			
+			$(document).scrollTop(initScrollTop - delta.y);
+			$(document).scrollLeft(initScrollLeft - delta.x);
+			
+		});
 		
-	}).on("mouseup mouseout touchend touchcancel touchleave", function(e) {
-		// Ending pan
-		if (edit_mode != "PANNING" || isCurrentlyPanning == false) return;
-		isCurrentlyPanning = false;
-		startPosition = null;
-	}).on("mousemove touchmove", function(e) {
-		// Perform the move
-		if (edit_mode != "PANNING" || isCurrentlyPanning == false) return;
-		
-		var currentPosition = (e.type.indexOf("mouse") > -1) ? new Point(e.clientX, e.clientY) : new Point(e.originalEvent.touches[0].clientX, e.originalEvent.touches[0].clientY);
-		
-		var delta = currentPosition.subtract(startPosition);
-		
-		$(document).scrollTop(initScrollTop - delta.y);
-		$(document).scrollLeft(initScrollLeft - delta.x);
-		
-	});
-	
-	// Hide the pan tool for the NetBoards, as the board is designed for their resolution
-	if (navigator.userAgent.indexOf("NetBoards/") > -1) {
+	} else {
+		// It is the NetBoard, so hide the pan tool
 		$("#pan").hide();
 	}
 });
