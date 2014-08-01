183c8467972e1034e51a99e4f10dc2669f11f8e5
Sam Haines <samjhaines@gmail.com>
2014-08-01 12:21:16 +0100

    Board render logging saves only changed boards

--- cgi-bin/render_rooms_logging.py
+++ cgi-bin/render_rooms_logging.py
@@ -7,19 +7,25 @@ from PIL import Image
 content_dir = os.path.join('..', 'content');
 archive_dir = os.path.join('..', 'content', 'archive');
 
+# cut-off time for new images
+# board JSON must be modified after this time in order to be rendered
+cutOff = datetime.datetime.now() - datetime.timedelta(hours=2)
 
 # what it does as a script
 if __name__ == "__main__":
 
     # get files which match format of room data: room_data_[r_id].json
     fs = [f for f in os.listdir(content_dir) if (f.startswith('room_data'))]
-
+    
     # extract room_ids
     pattern = 'room_data_(.+)\.json'
     room_ids = [re.search(pattern, f).group(1).lower() for f in fs]
-
+    
+    # restrict to rooms with changes after cut-off time
+    room_ids_with_changes = [r for r in room_ids if datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(content_dir, r + ".json"))) > cutOff]
+    
     # call phantomjs
-    subprocess_arr = [os.path.join(os.getcwd(), "phantomjs"), "--ignore-ssl-errors=true", "render_netboard.js"] + room_ids
+    subprocess_arr = [os.path.join(os.getcwd(), "phantomjs"), "--ignore-ssl-errors=true", "render_netboard.js"] + room_ids_with_changes
     print "Starting subprocess: " + " ".join(subprocess_arr)
     subprocess.call(subprocess_arr, shell=(os.name == "nt"))
 	
