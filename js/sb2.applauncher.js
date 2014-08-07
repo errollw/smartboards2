39660bded748e5a5e3187b34e2de924fd4549d8d
Sam Haines <samjhaines@gmail.com>
2014-08-07 15:56:00 +0100

    App Launcher

---
+++ js/sb2.applauncher.js
@@ -0,0 +1,21 @@
+// Shows the button to launch an app if this board is on the whitelist
+
+$(function() {   
+
+    var enabled_rooms = [ "r_ss12" ];
+
+    if (enabled_rooms.indexOf(r_id) > -1) {
+        $controls = $("#controls");
+        $button = $("<div />").addClass("button").attr("id", "applauncher").html("Start<br />App").css({
+            "font-weight":"bold",
+            "text-align":"center",
+            "line-height":"30px",
+            "padding":"10px 0",
+            "height":"60px"
+        }).on("click", function() {
+            var img = new Image();
+            img.src = "http://localhost:8080/?rand=" + Math.random();
+        });
+        $("<section />").append($button).appendTo($controls);
+    }
+});
