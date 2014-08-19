5fb9006de80d9ad7682bbd2a9253062820a8196b
Sam Haines <samjhaines@gmail.com>
2014-08-19 16:12:11 +0100

    Floatie improvements and scroll enabled

--- js/sb2.floatie.js
+++ js/sb2.floatie.js
@@ -73,9 +73,72 @@ function hide_floatie(){
 }
 
 function show_floatie(point){
+    var fHeight = $("#floatie").height(), fWidth = $("#floatie").width(), triangleDim = $("#floatie-triangle").height()/2 * 1.4142; // triangle dim is the height of the triangle (or half-height of the diamond). jQuery's height() returns the height of the <div> before rotation
+    
+    var minimumMargin = 30, sideGuide = minimumMargin + fWidth/2, topMargin = minimumMargin + fHeight/2, bottomMargin = minimumMargin + fHeight + triangleDim, triangleSide = "TOP", targetPosition = point.clone();
+    
+    if (targetPosition.x < minimumMargin) {
+        targetPosition.x = minimumMargin;
+        triangleSide = "LEFT";
+    } else if (minimumMargin <= targetPosition.x && targetPosition.x < sideGuide) {
+        triangleSide = "LEFT";
+    } else if (view.bounds.width - sideGuide < targetPosition.x && targetPosition.x <= view.bounds.width - minimumMargin) {
+        triangleSide = "RIGHT";
+    } else if (view.bounds.width - minimumMargin < targetPosition.x) {
+        targetPosition.x = view.bounds.width - minimumMargin;
+        triangleSide = "RIGHT";
+    }
+    
+    function fixX() {
+        targetPosition.x = Math.min(view.bounds.x + view.bounds.width - sideGuide, Math.max(view.bounds.x + sideGuide, targetPosition.x));
+    }
+    
+    if (targetPosition.y < topMargin) {
+        fixX();
+        targetPosition.y = minimumMargin;
+        triangleSide = "TOP";
+    } else if (view.bounds.height - bottomMargin < targetPosition.y && targetPosition.y < view.bounds.height - minimumMargin) {
+        fixX();
+        triangleSide = "BOTTOM";
+    } else if (view.bounds.height - minimumMargin < targetPosition.y) {
+        fixX();
+        targetPosition.y = view.bounds.height - minimumMargin;
+        triangleSide = "BOTTOM";
+    }
+    
+    var floatiePoint, trianglePoint;
+    switch (triangleSide) {
+        case "TOP":
+            floatiePoint = targetPosition.clone();
+            trianglePoint = targetPosition.clone();
+            if (targetPosition.x != point.x) { // Slide the triangle to point towards the selection (important when in corners of screen)
+                trianglePoint.x = Math.max(minimumMargin + 2*triangleDim, Math.min(point.x, view.bounds.width - minimumMargin - 2*triangleDim));
+            }
+            break;
+        case "LEFT":
+            floatiePoint = targetPosition.clone().add(new Point(fWidth/2 + triangleDim, -triangleDim - fHeight/2));
+            trianglePoint = floatiePoint.clone().add(new Point(-fWidth/2, fHeight/2));
+            break;
+        case "RIGHT":
+            floatiePoint = targetPosition.clone().add(new Point(-fWidth/2 - triangleDim, -triangleDim - fHeight/2));
+            trianglePoint = floatiePoint.clone().add(new Point(fWidth/2, fHeight/2));
+            break;
+        case "BOTTOM":
+            floatiePoint = targetPosition.clone().add(new Point(0, -fHeight - triangleDim*2));
+            trianglePoint = floatiePoint.clone().add(new Point(0, fHeight));
+            if (targetPosition.x != point.x) { // Slide the triangle to point towards the selection (important when in corners of screen)
+                trianglePoint.x = Math.max(minimumMargin + 2*triangleDim, Math.min(point.x, view.bounds.width - minimumMargin - 2*triangleDim));
+            }
+            break;
+    }
+    
 	$('#floatie, #floatie-triangle').show();
-	$('#floatie, #floatie-triangle').css({
-		top: point.y,
-		left: point.x
+	$('#floatie').css({
+		top: floatiePoint.y,
+		left: floatiePoint.x
 	});
+    $('#floatie-triangle').css({
+        top: trianglePoint.y,
+        left: trianglePoint.x
+    });
 }
