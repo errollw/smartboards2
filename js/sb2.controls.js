b2a56eee477a2d7ca355c54931ac2928e498d04d
Sam Haines <samjhaines@gmail.com>
2014-09-02 12:27:32 +0100

    Osterei #2

--- js/sb2.controls.js
+++ js/sb2.controls.js
@@ -13,6 +13,17 @@ var moveSelectedItems = function (d) {
     }
 }
 
+var keyPressBuffer = "";
+var keyboardMap = ["","","","CANCEL","","","HELP","","BACK_SPACE","TAB","","","CLEAR","ENTER","RETURN","","SHIFT","CONTROL","ALT","PAUSE","CAPS_LOCK","KANA","EISU","JUNJA","FINAL","HANJA","","ESCAPE","CONVERT","NONCONVERT","ACCEPT","MODECHANGE","SPACE","PAGE_UP","PAGE_DOWN","END","HOME","LEFT","UP","RIGHT","DOWN","SELECT","PRINT","EXECUTE","PRINTSCREEN","INSERT","DELETE","","0","1","2","3","4","5","6","7","8","9","COLON","SEMICOLON","LESS_THAN","EQUALS","GREATER_THAN","QUESTION_MARK","AT","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","WIN","","CONTEXT_MENU","","SLEEP","NUMPAD0","NUMPAD1","NUMPAD2","NUMPAD3","NUMPAD4","NUMPAD5","NUMPAD6","NUMPAD7","NUMPAD8","NUMPAD9","MULTIPLY","ADD","SEPARATOR","SUBTRACT","DECIMAL","DIVIDE","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","F13","F14","F15","F16","F17","F18","F19","F20","F21","F22","F23","F24","","","","","","","","","NUM_LOCK","SCROLL_LOCK","WIN_OEM_FJ_JISHO","WIN_OEM_FJ_MASSHOU","WIN_OEM_FJ_TOUROKU","WIN_OEM_FJ_LOYA","WIN_OEM_FJ_ROYA","","","","","","","","","","CIRCUMFLEX","EXCLAMATION","DOUBLE_QUOTE","HASH","DOLLAR","PERCENT","AMPERSAND","UNDERSCORE","OPEN_PAREN","CLOSE_PAREN","ASTERISK","PLUS","PIPE","HYPHEN_MINUS","OPEN_CURLY_BRACKET","CLOSE_CURLY_BRACKET","TILDE","","","","","VOLUME_MUTE","VOLUME_DOWN","VOLUME_UP","","","","","COMMA","","PERIOD","SLASH","BACK_QUOTE","","","","","","","","","","","","","","","","","","","","","","","","","","","OPEN_BRACKET","BACK_SLASH","CLOSE_BRACKET","QUOTE","","META","ALTGR","","WIN_ICO_HELP","WIN_ICO_00","","WIN_ICO_CLEAR","","","WIN_OEM_RESET","WIN_OEM_JUMP","WIN_OEM_PA1","WIN_OEM_PA2","WIN_OEM_PA3","WIN_OEM_WSCTRL","WIN_OEM_CUSEL","WIN_OEM_ATTN","WIN_OEM_FINISH","WIN_OEM_COPY","WIN_OEM_AUTO","WIN_OEM_ENLW","WIN_OEM_BACKTAB","ATTN","CRSEL","EXSEL","EREOF","PLAY","ZOOM","","PA1","WIN_OEM_CLEAR",""];
+var developerMode = false;
+
+// String.endsWith function - http://stackoverflow.com/a/2548133
+if (typeof String.prototype.endsWith !== 'function') {
+    String.prototype.endsWith = function(suffix) {
+        return this.indexOf(suffix, this.length - suffix.length) !== -1;
+    };
+}
+
 $(document).on("keydown", function (evt) {
     
     // Cancel the default behaviour if this gets set to true
@@ -90,6 +101,12 @@ $(document).on("keydown", function (evt) {
         }
     } else if (!evt.ctrlKey && evt.altKey) {
         // Alt key only
+        switch(evt.keyCode) {
+            case 88: // X key for Alt-X developer mode
+                developerMode = true;
+                dealtWith = true;
+                break;
+        }
     } else {
         // No modifiers
         switch(evt.keyCode) {
@@ -190,9 +207,26 @@ $(document).on("keydown", function (evt) {
     if (dealtWith == true) {
         view.update();
         evt.preventDefault();
+        keyPressBuffer = "";
         return false;
     } else {
-        console.log("Keyboard event not captured: keyCode=" + evt.keyCode);
+        keyPressBuffer += keyboardMap[evt.keyCode] + "|";
+        if (keyPressBuffer.endsWith("B|A|N|A|N|A|S|")) {
+            $("div.profile-pic").css("background-image", "url('http://l-userpic.livejournal.com/8052766/396814')");
+            var names = $("header").has(".title");
+            for (var i = 0; i < names.length; i++) {
+                var titleField = $(names[i]).find(".title"), subtitleField = $(names[i]).find(".subtitle");
+
+                if (subtitleField.text().length > 0) {
+                    subtitleField.text(subtitleField.text() + " on the Moon for " + titleField.text().length + " years");
+                }
+                titleField.text(titleField.text().replace(/ /, ' "The Banana" '));
+            }
+            keyPressBuffer = "";
+        }
+        if (developerMode) {
+            console.log("Keyboard event not captured: keyCode=" + evt.keyCode, "Key press buffer is: " + keyPressBuffer);
+        }
     }
 });
 
